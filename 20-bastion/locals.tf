69d6b34f9cdee73ae139d2ad2d0f90b0139c3727
SSR <someshwar.sangaraju19@gmail.com>
2026-03-11 22:48:47 +0530

    terraform

--- 20-bastion/locals.tf
+++ 20-bastion/locals.tf
@@ -1,11 +1,11 @@
 locals{
     ami_id = data.aws_ami.joindevops.id
     bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
-    public_subnet_ids = split(",",data.aws_ssm_parameter.public_subnet_ids.value)[0]
+    public_subnet_id = split("," , data.aws_ssm_parameter.public_subnet_ids.value)[0]
     common_tags={
-        Project = "Expense"
-        Environment = "dev"
-        Terraform ="true"
+        Project = var.project
+        Environment = var.environment
+        Terraform = "true"
     }
     common_suffix_name = "${var.project}-${var.environment}"
 }
--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -12,8 +12,8 @@
 resource "aws_instance" "bastion" {
   ami                    = local.ami_id
   instance_type          = "t3.micro"
-  subnet_id              = local.public_subnet_ids
   vpc_security_group_ids = [local.bastion_sg_id]
+  subnet_id              = local.public_subnet_id
 
   tags = {
     Name = "${local.common_suffix_name}-bastion"
