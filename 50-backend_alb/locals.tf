a24d69e6b23b973f7e9d6599e07f9542ae8d3cbe
SSR <someshwar.sangaraju19@gmail.com>
2026-03-13 21:51:01 +0530

    Expense infra

--- 50-backend_alb/locals.tf
+++ 50-backend_alb/locals.tf
@@ -2,8 +2,6 @@ locals{
 
     backend_alb_sg_id = data.aws_ssm_parameter.backend_alb.arn
     private_subnet_ids= split("," , data.aws_ssm_parameter.private_subnet_ids.value)
-    public_subnet_ids= split("," , data.aws_ssm_parameter.public_subnet_ids.value)
-    database_subnet_ids= split("," , data.aws_ssm_parameter.database_subnet_ids.value)
 
     tags = {
         Project = "Roboshop"
