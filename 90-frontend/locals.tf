79998bbb8fa66af36c6ca16f6d7b85afe9dbdc68
SSR <someshwar.sangaraju19@gmail.com>
2026-03-17 12:23:13 +0530

    Expense infra

--- 90-frontend/locals.tf
+++ 90-frontend/locals.tf
@@ -2,7 +2,8 @@ locals{
     common_suffix_name = "${var.project}-${var.environment}"
     ami_id = data.aws_ami.joindevops.id
     frontend_sg_id = data.aws_ssm_parameter.frontend_sg_id.value
-    private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
     common_tags = {
